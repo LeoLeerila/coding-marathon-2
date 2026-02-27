335c638bbad4c30f7ae85268c0f5362801e397b5
Topiuhm <makinen.eeli03@gmail.com>
2026-02-27 14:28:08 +0200

    (chore) change signup form formatting

--- frontend-simplified/src/pages/Signup.jsx
+++ frontend-simplified/src/pages/Signup.jsx
@@ -35,7 +35,7 @@ export default function Signup() {
             })
         })
         const user = await res.json();
-        if(!res.ok) {
+        if (!res.ok) {
             console.log("nu uh")
             return
         }
@@ -48,21 +48,52 @@ export default function Signup() {
             <h2 className="text-3xl text-center font-semibold mb-6">Signup</h2>
             <form className='text-center flex flex-col align-middle justify-center items-center h-full'>
                 <label className="block text-gray-700 font-bold mb-2">Name:</label>
-                <input className="border roundedl py-2 px-3" type="text" required value={name} onChange={(e) => setName(e.target.value)} />
+                <input className="border roundedl py-2 px-3"
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">Email:</label>
-                <input className="border rounded py-2 px-3" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">Password:</label>
-                <input className="border rounded py-2 px-3" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">Phone number:</label>
-                <input className="border rounded py-2 px-3" type="text" required value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="text"
+                    required
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">Gender:</label>
-                <input className="border rounded py-2 px-3" type="text" required value={gender} onChange={(e) => setGender(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="text"
+                    required
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">Street:</label>
-                <input className="border rounded py-2 px-3" type="text" required value={street} onChange={(e) => setStreet(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="text"
+                    required value={street}
+                    onChange={(e) => setStreet(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">City:</label>
-                <input className="border rounded py-2 px-3" type="text" required value={city} onChange={(e) => setCity(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="text"
+                    required
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)} />
                 <label className="block text-gray-700 font-bold mb-2">Zip Code:</label>
-                <input className="border rounded py-2 px-3" type="text" required value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+                <input className="border rounded py-2 px-3"
+                    type="text"
+                    required
+                    value={zipCode}
+                    onChange={(e) => setZipCode(e.target.value)} />
                 <button onClick={(e) => {
                     signupUser(e);
                 }} className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded mt-4">Signup</button>
